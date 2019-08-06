d338a3172868dd9e5507db94323ccd52ff005bbf
itohiroki0527 <itohiroki0527@gmail.com>
2019-08-06 14:38:16 +0900

    Bブランチにコミットします

--- first.java
+++ first.java
@@ -2,7 +2,7 @@ public class present {
 
   public static void main(String[] args) {
     System.out.println("HelloWorld");
-  	System.out.println("‚ ‚¢‚¤‚¦‚¨");
+  	System.out.println("Hello!");
   }
 
 }
